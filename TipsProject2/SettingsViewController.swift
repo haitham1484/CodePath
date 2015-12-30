ce1deca04ea89cf6a610d929606ee6c0461bb08d
Kenway <kenway@Kenways-MacBook-Pro.local>
2015-12-30 03:56:04 -0500

    Here is my commit message

---
+++ TipsProject2/SettingsViewController.swift
@@ -0,0 +1,154 @@
+//
+//  SettingsViewController.swift
+//  TipsProject2
+//
+//  Created by Kenway on 12/29/15.
+//  Copyright © 2015 haitham mawari. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var switch1: UISwitch!
+    @IBOutlet var switch2: UISwitch!
+    @IBOutlet var switch3: UISwitch!
+    
+    
+    @IBOutlet weak var switchNumberTwo: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+            
+        if (defaults.objectForKey("SwitchState") != nil) {
+            switch1.on = defaults.boolForKey("SwitchState")
+        }
+        
+        
+        if (defaults.objectForKey("SwitchState2") != nil) {
+            switch2.on = defaults.boolForKey("SwitchState2")
+        }
+        
+        
+        if (defaults.objectForKey("SwitchState3") != nil) {
+            switch3.on = defaults.boolForKey("SwitchState3")
+        }
+        
+       
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTap(sender: UISwitch) {
+    
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(0.18, forKey: "default_tip_percentage")
+        defaults.synchronize()
+        
+        
+        
+        print("blue")
+    
+    }
+    
+    
+
+    @IBAction func onTap2(sender: UISwitch) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(0.20, forKey: "default_tip_percentage")
+        defaults.synchronize()
+        
+        print("yellow")
+
+    }
+    
+    @IBAction func onTap3(sender: UISwitch) {
+  
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(0.22, forKey: "default_tip_percentage")
+        defaults.synchronize()
+
+        print("red")
+        
+       
+        
+      
+        
+        defaults.synchronize()
+        
+        print("red")
+
+        
+        
+        
+        
+    }
+    
+    
+    
+    @IBAction func saveSwitchState1(sender: AnyObject) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if switch1.on {
+            defaults.setBool(true, forKey: "SwitchState")
+        } else {
+            defaults.setBool(false, forKey: "SwitchState")
+        }
+    }
+    
+    @IBAction func saveSwitchState2(sender: AnyObject) {
+        
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if switch2.on {
+            defaults.setBool(true, forKey: "SwitchState2")
+        } else {
+            defaults.setBool(false, forKey: "SwitchState2")
+        }
+
+    }
+    
+    @IBAction func saveSwitchState3(sender: AnyObject) {
+        
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if switch3.on {
+            defaults.setBool(true, forKey: "SwitchState3")
+        } else {
+            defaults.setBool(false, forKey: "SwitchState3")
+        }
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        let defaults2 = NSUserDefaults.standardUserDefaults()
+        
+        
+        print("view will appear 2")
+            }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TipsProject2/ViewController.swift
+++ TipsProject2/ViewController.swift
@@ -10,16 +10,171 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var splitValueField: UITextField!
+    @IBOutlet weak var eachValue: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var firstView: UISegmentedControl!
+    @IBOutlet weak var secondView: UISegmentedControl!
+    @IBOutlet weak var secondVeiwTechLogo: UIImageView!
+    @IBOutlet weak var firstViewTechLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.firstView.alpha = 1
+        self.secondView.alpha = 1
+        
+        
+        UIView.animateWithDuration(3, animations: {
+            // This causes first view to fade in and second view to fade out
+            self.firstView.alpha = 0
+            self.secondView.alpha = 1
+            
+            self.firstViewTechLogo.alpha = 0
+            self.secondVeiwTechLogo.alpha = 1
+            
+            
+        })
+        
+        
+
+        
+        
+                // Sets the title in the Navigation Bar
+        self.title = "The most generous Tip Calculator"
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "0.00"
+        totalLabel.text = "0.00"
+        
+        splitValueField.text = "1"
     }
+    
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.20, 0.22]
+        
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        
+        var splitField = NSString(string: splitValueField.text!).doubleValue
+        
+        var tip = (billAmount * tipPercentage)
+        
+        var total = billAmount + tip
+        
+        var totalEach = total/splitField
+       
+        
+        tipLabel.text = "$\(tip)"
+        
+        totalLabel.text = "$\(total)"
+    
+        
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+        
+       
+        
+        eachValue.text = "$\(totalEach)"
+        
+        eachValue.text = String(format: "%.2f", totalEach)
+            
+        
+        
+        
+        print("user editing bill")
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+        
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipValue = defaults.doubleForKey("default_tip_percentage")
+       
+        
+        print("view will appear")
+     
+        var tipPercentage = tipValue
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        
+        var tip = billAmount * tipPercentage
+        
+        var total = billAmount + tip
+        
+        
+        
+        
+        tipLabel.text = "$\(tip)"
+        
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+        print("user editing bill")
+        
+        
+        var splitField = NSString(string: splitValueField.text!).doubleValue
+
+        
+        var totalEach = total/splitField
+        
+        eachValue.text = "$\(totalEach)"
+        
+        eachValue.text = String(format: "%.2f", totalEach)
+
+        
+        // This is a good place to retrieve the default tip percentage from NSUserDefaults
+        // and use it to update the tip amount
+        
+    }
+    
+
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    
+    // Optionally initialize the property to a desired starting value
+   
+    
 
 }
 
